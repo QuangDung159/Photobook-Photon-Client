8616b9edddebb0e2693fb62b5eb5cf93b45e7a36
DungLu <quangdunglu159@gmail.com>
2020-06-30 12:21:26 +0700

    Add vuex, function show list album

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
-import Vuex from "vuex";
 
 import routes from './router';
 import store from "./stores/store";
@@ -9,7 +8,6 @@ import store from "./stores/store";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(Vuex);
 
 const router = new VueRouter({
   routes
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,10 +1,45 @@
-const store = {
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from 'axios';
+
+import config from "../../config";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
     state: {
-        result: 123123
+        result: 3432,
+        listAlbum: []
     },
     mutations: {
+        increment(state, payload) {
+            state.result += payload.amount;
+        },
 
+        getListAlbum(state) {
+            axios.get(config.API_URL + '/album/get-all')
+                .then(res => {
+                    let data = res.data;
+                    if (data.statusCode === 200) {
+                        state.listAlbum = data.listAlbum;
+                    } else {
+                        console.log(data.message);
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    actions: {
+        increment({ commit }, payload) {
+            commit('increment', payload);
+        },
+
+        getListAlbum({ commit }) {
+            commit('getListAlbum');
+        }
     }
-};
+});
 
 export default store;
